2454b25b47334814cc233b2af00f6bcbf4199359
sergey.kr <sergey.kr@onyxenterprises.com>
2016-09-21 23:45:47 +0300

    Initial commit

---
+++ src/components/Article.js
@@ -0,0 +1,68 @@
+import React, { PropTypes, Component } from 'react'
+import { findDOMNode } from 'react-dom'
+import CommentList from './CommentList'
+import toggleOpen from '../decorators/toggleOpen'
+import { deleteArticle } from '../AC/articles'
+import { commentStore } from '../stores'
+
+class Article extends Component {
+    constructor() {
+        super()
+        this.state = {
+            some: ''
+        }
+    }
+
+    componentWillMount() {
+    }
+
+    componentDidMount() {
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    componentDidUpdate() {
+    }
+
+    render() {
+        const { article, isOpen, toggleOpen } = this.props
+        if (!article) return <h3>No article</h3>
+
+        const { title, text, comments, id } = article
+        const textItem = isOpen ? <section>
+            {text}
+            <div><CommentList comments = {article.getRelation('comments')} ref="list" /></div>
+        </section> : null
+        return (
+            <div>
+                <h3 onClick = {toggleOpen} ref="title">
+                    {title}
+                    <a href="#" onClick={this.handleDelete}>delete me</a>
+                </h3>
+                {textItem}
+            </div>
+        )
+    }
+
+    handleDelete = (ev) => {
+        ev.preventDefault()
+        deleteArticle(this.props.article.id)
+    }
+}
+
+
+Article.propTypes = {
+    article: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        text: PropTypes.string,
+        id: PropTypes.string.isRequired
+    }),
+
+    //From toggleOpen decorator
+    isOpen: PropTypes.bool.isRequired,
+    toggleOpen: PropTypes.func.isRequired
+}
+
+export default Article
